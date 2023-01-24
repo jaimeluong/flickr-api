b69f777d599e6504b52a13a194ebf42d78a685cb
Jaime Luong <jaime.luong@gmail.com>
2023-01-23 23:43:13 -0600

    Last 5 images appear, add style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
         if params[:username]
             @id = @flickr.people.findByUsername(username: params[:username])['nsid']
-            @photos = @flickr.people.getPublicPhotos(user_id: @id, per_page: 5)
+            @photos = @flickr.people.getPublicPhotos(user_id: @id, per_page: 5, extras: "url_m, url_s")
         end
     end
 end
